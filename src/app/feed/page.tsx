a67615e1799a64395232771919d9d93999895581
Gustavo <gustavo.h.a.cavalcanti@gmail.com>
2025-02-04 00:17:20 -0300

    página inicial

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -17,7 +17,25 @@ export default function FeedPage() {
                 </div>
                 <img src="/ImagemBanner.png" alt="Imagem frutas" className="w-full h-[500px] object-cover mt-10" />
             </section>
+            <section className="bg-white py-16 px-6">
+                <div className="container mx-auto flex flex-col md:flex-row items-center gap-8">
+                    <div>
+                    <img src="/ImagemLoc.png" alt="Localização" className="w-[240px] h-[200px] rounded-lg shadow-lg ml-[202px]" />
+                    </div>
+                    <div className="w-full md:w-1/2 text-center md:text-left flex flex-col items-center justify-center">
+                        <h2 className="text-4xl font-bold text-[#E02222] mb-4">
+                            Localização
+                        </h2>
+                        <p className="text-lg text-gray-700 mb-6 text-center">
+                            Compartilhe sua localização para que possamos recomendar as melhores lojas da nossa feira para você comprar
+                        </p>
 
+                        <button className="bg-red-600 text-white px-6 py-3 rounded-full text-lg font-semibold hover:bg-red-700 transition duration-300">
+                            Informe sua Localização
+                        </button>
+                    </div>
+                </div>
+            </section>
         </div>
 
     );
