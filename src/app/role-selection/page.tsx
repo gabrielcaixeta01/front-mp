8a73dfd913c246f3b7b5bf1a17b222a813604844
gabrielcaixeta01 <gabrielcaixeta@Laptop-de-Gabriel.local>
2025-02-05 15:33:04 -0300

    Formulario de Role

--- src/app/role-selection/page.tsx
+++ src/app/role-selection/page.tsx
@@ -17,33 +17,43 @@ export default function RoleSelection() {
     };
 
     return (
-        <div className="flex h-60 items-center w-full justify-center min-h-screen bg-gray-100 p-4">
-            <div className="w-full max-w-sm bg-gray-300 rounded-2xl p-6 shadow-lg text-center relative">
-                <button 
+        <div className="flex items-center w-full justify-center min-h-screen bg-white p-4">
+            <div className="w-full max-w-[20%] min-h-fit bg-gray-200 py-10 rounded-2xl shadow-lg text-center relative">
+                <div>
+                    <button 
                     onClick={() => router.push("/feed")} 
-                    className="absolute top-4 left-4 bg-black text-white px-4 py-2 rounded-full text-sm"
-                >
-                    Voltar
-                </button>
-                <h1 className="text-2xl font-bold mb-6">Quem é você?</h1>
-                <button 
-                    onClick={() => handleSelection("cliente")}
-                    className="w-full px-6 py-3 bg-red-600 text-white rounded-xl hover:bg-red-700 transition mb-3"
-                >
-                    Sou cliente
-                </button>
-                <button 
-                    onClick={() => handleSelection("vendedor")}
-                    className="w-full px-6 py-3 bg-red-600 text-white rounded-xl hover:bg-red-700 transition mb-3"
-                >
-                    Sou vendedor
-                </button>
-                <button 
-                    onClick={() => handleSelection("administrador")}
-                    className="w-full px-6 py-3 bg-red-600 text-white rounded-xl hover:bg-red-700 transition"
-                >
-                    Sou administrador
-                </button>
+                    className="absolute top-4 left-4 bg-white text-black hover:bg-black hover:text-white transition duration-500 px-4 py-1 rounded-full text-sm"
+                    >
+                        Voltar
+                    </button> 
+                </div>
+
+                <div className="flex flex-col items-center">
+                    <h1 className="text-2xl font-semibold text-black mt-6">Quem é você?</h1>
+                    <p className="text-gray-500 text-sm mb-6">Selecione como deseja usar nosso site.</p>
+                    <div className="w-full px-16 py-3 flex flex-col items-center">
+                        <button 
+                            onClick={() => handleSelection("cliente")}
+                            className="w-full px-6 py-3 bg-white text-red-500 rounded-xl hover:bg-red-500 hover:text-white transition duration-500 mb-3"
+                        >
+                            Sou cliente
+                        </button>
+                        <button 
+                            onClick={() => handleSelection("vendedor")}
+                            className="w-full px-6 py-3 bg-white text-red-500 rounded-xl hover:bg-red-500 hover:text-white transition duration-500 mb-3"
+                        >
+                            Sou vendedor
+                        </button>
+                        <button 
+                            onClick={() => handleSelection("administrador")}
+                            className="w-full px-6 py-3 bg-white text-red-500 rounded-xl hover:bg-red-500 hover:text-white transition duration-500 mb-3"
+                        >
+                            Sou administrador
+                        </button>
+                    </div>
+                </div>
+                
+            
             </div>
         </div>
     );
